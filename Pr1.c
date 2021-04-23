32cd0732784f277f3bc3512eccf20f325e769120
Sudha-0023 <72972805+Sudha-0023@users.noreply.github.com>
2021-04-24 01:07:13 +0530

    Update Pr1.c

--- Pr1.c
+++ Pr1.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  FILE *fptr;
+  char buf[100],c;
+  
+  printf("enter the name of the file to open:\n");
+  scanf("%s",&buf);
+  
+  fptr=fopen(buf,"r");
+  if(fptr==NULL)
+  {
+    printf("The file is not available.\n");
+    exit(0);
+  }
+  c=fgetc(fptr);
+  while(c!=EOF)
+  {
+    printf("%c",c);
+    c=fgetc(fptr);
+  }
+  printf("\n");
+  
+  fclose(fptr);
+  return 0;
+
+}
 
